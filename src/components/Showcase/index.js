cbdb69bebd9db4e8d6654d9717e1b8f295a02329
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-26 13:20:31 +0000

    Added ArrowDown component

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import video from "../../assets/beta.mp4";
 import NavBar from '../NavBar';
-import { Overlay, Video, Text, TextBig, TextSmall } from './styled';
+import { Overlay, Video, Text, TextBig, TextSmall, ArrowDown, Arrow } from './styled';
 
 
 const Showcase = () => {
+
+
     return (
         <>
-        <NavBar />
-        <Video src={video} autoPlay muted loop />
-        <Overlay />
-        <Text>
-            <TextBig>Hi, I'm Kacper and I can help you</TextBig>
-            <TextSmall>take nice photos.</TextSmall>
-        </Text>
-       </> 
+            <NavBar />
+            <Video src={video} autoPlay muted loop />
+            <Overlay />
+            <Text>
+                <TextBig>Hi, I'm Kacper and I can help you</TextBig>
+                <TextSmall>take nice photos.</TextSmall>
+                
+            </Text>
+            <ArrowDown class="arrow bounce">
+                    <Arrow href="#"><i class="fas fa-chevron-down"></i></Arrow>
+            </ArrowDown>
+            
+        </>
     )
 }
 

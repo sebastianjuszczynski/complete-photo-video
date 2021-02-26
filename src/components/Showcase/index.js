54442ef2d76207605aa9de85da5f2bccabbfcbe3
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-26 20:45:57 +0000

    typewriting effect added

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import video from "../../assets/beta.mp4";
 import NavBar from '../NavBar';
 import { Overlay, Video, Text, TextBig, TextSmall, ArrowDown, Arrow } from './styled';
-
-
+import Typewriter from 'typewriter-effect';
 const Showcase = () => {
 
 
@@ -13,14 +12,42 @@ const Showcase = () => {
             <Video src={video} autoPlay muted loop />
             <Overlay />
             <Text>
-                <TextBig>Hi, I'm Kacper and I can help you</TextBig>
-                <TextSmall>take nice photos.</TextSmall>
-                
+                <TextBig>
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter.typeString("Hi, I'm Kacper and I can help you")
+                                .pauseFor(2500)
+                                .start();
+                        }}
+                    />
+                </TextBig>
+                <TextSmall>
+                    <Typewriter
+                          options={{
+                            loop: true,
+                          }}
+                        onInit={(typewriter) => {
+                            typewriter
+                                .pauseFor(4500)
+                                .typeString("take nice photos.")
+                                .pauseFor(2500)
+                                .deleteAll()
+                                .typeString("create a beautiful logo.")
+                                .pauseFor(2500)
+                                .deleteAll()
+                                .typeString("make a cool video.")
+                                .pauseFor(2500)
+                                .deleteAll()
+                                .start();
+                        }}
+                    />
+                </TextSmall>
+
             </Text>
             <ArrowDown class="arrow bounce">
-                    <Arrow href="#"><i class="fas fa-chevron-down"></i></Arrow>
+                <Arrow href="#"><i class="fas fa-chevron-down"></i></Arrow>
             </ArrowDown>
-            
+
         </>
     )
 }

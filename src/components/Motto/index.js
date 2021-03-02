1fbfe0412b8a5adcf25f4be1bd6c0cbc9f03f28c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-03-02 20:12:36 +0000

    Motto created and styled

---
+++ src/components/Motto/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from './styled'
+
+const Motto = () => {
+    return (
+        <Text>
+            Obraz jest tak samo ważny jak tysiąc<br></br> słów, więc sprawdź kilka moich <br></br>projektów.
+        </Text>
+    )
+}
+
+export default Motto
---
+++ src/components/Motto/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Text = styled.h2`
+    margin: 100px auto;
+    text-align: center;
+    text-transform: uppercase;
+    line-height: 1.3;
+    font-size: 40px;
+    /* font-weight: 400; */
+`;

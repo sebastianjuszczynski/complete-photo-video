e72707bfbd6f1ef904304abecdb52262a5c4eba9
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-03-02 20:12:51 +0000

    Motto added to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+
+import Motto from "./components/Motto";
 import Showcase from "./components/Showcase";
 
 
 function App() {
   return (
+    <>
     <Showcase />
+    <Motto />
+    </>
   );
 }
 

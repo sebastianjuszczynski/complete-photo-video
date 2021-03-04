68b27d047f449a35faf9b05562d72fabed4e4194
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-03-04 17:38:04 +0000

    FormHeader created, styled and added to Form

---
+++ src/components/FormHeader/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FormH3, FormSpan } from './styled';
+
+const FormHeader = () => {
+    return (
+        <FormH3>Skontaktuj się <FormSpan>ze mną</FormSpan> i sprawdź<br />co możemy zrobić <FormSpan>razem!</FormSpan></FormH3>
+    )
+}
+
+export default FormHeader;
---
+++ src/components/FormHeader/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FormH3 = styled.h3`
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 200;
+    font-size: 28px;
+`;
+export const FormSpan = styled.span`
+    font-weight: bold;
+`;

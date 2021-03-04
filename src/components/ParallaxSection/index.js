55f2d6884c7dfab95c76dfd91f140e2cc251921f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-03-04 12:40:05 +0000

    ParallaxSection created and styled

---
+++ src/components/ParallaxSection/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Parallax } from 'react-parallax';
+import photographer from "../../assets/photographer.jpg"
+import { ParallaxContainer, TextContainer, TextH3, TextParagraph } from './styled';
+
+
+const ParallaxSection = () => {
+    return (
+        <ParallaxContainer>
+            <Parallax bgImage={photographer} bgImageAlt="photographer" strength={150}
+            style={{position: "relative"}}
+            bgImageStyle={{zIndex: "-1"}}
+            >
+                <TextContainer style={{ height: 400 }}>
+                    <TextH3>Troszeczkę o mnie</TextH3>
+                    <TextParagraph>Moją główną inspiracją do tworzenia filmów i robienia zdjęć są podróże.<br></br> Urodziłem się w Polsce, ale zwiedziłem wiele zakątków świata.<br></br> Obecnie mieszkam w Milton Keynes - mieście, które ciągle jest w ruchu,<br></br> tak jak ja. </TextParagraph>
+                </TextContainer>
+            </Parallax>
+        </ParallaxContainer>
+    )
+}
+
+export default ParallaxSection;
---
+++ src/components/ParallaxSection/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const TextContainer = styled.div`
+    position: relative;
+    top: 100px;
+    left: 50%;
+    color: white;
+`;
+
+export const TextH3 = styled.h3`
+    font-size: 36px;
+`;
+
+export const TextParagraph = styled.p`
+    font-size: 20px;
+    line-height: 1.5;
+`;
+export const ParallaxContainer = styled.div`
+    background: rgba(0, 0, 0, 0.4);
+`;

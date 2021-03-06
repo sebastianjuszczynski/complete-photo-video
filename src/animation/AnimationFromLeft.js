d9b9ffe974aabde9881e538c629e410c5e9eaa04
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-03-06 15:47:27 +0000

    Basic animation created just check if works

---
+++ src/animation/AnimationFromLeft.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+
+const AnimationFromLeft = ({children}) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
+
+    return (
+        <motion.p
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            transition={{ duration: 1 }}
+            variants={{
+                visible: { x: 0},
+                hidden: { x: -1000}
+            }}
+        >
+            {children}
+        </motion.p>
+    )
+}
+
+export default AnimationFromLeft;
